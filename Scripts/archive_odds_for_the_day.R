2cfc73847a0c652d0c69d3da2e5863fc300f1bd1
James Brown <james.brown1417@gmail.com>
2023-10-26 17:01:40 +1030

    odds archive

---
+++ Scripts/archive_odds_for_the_day.R
@@ -0,0 +1,20 @@
+#===============================================================================
+# Libraries and functions
+#===============================================================================
+
+library(tidyverse)
+library(glue)
+`%notin%` <- Negate(`%in%`)
+
+#===============================================================================
+# Read in scraped odds
+#===============================================================================
+
+# Get all scraped odds files and combine
+all_odds_files <-
+  list.files("Data/scraped_odds", full.names = TRUE) |>
+  map(read_csv) |>
+  reduce(bind_rows) |> 
+  mutate(timestamp = Sys.time()) |> 
+  relocate(timestamp, .before = match) |> 
+  write_csv(glue("Data/odds_archive_scraped_odds_{Sys.Date()}.csv"))
