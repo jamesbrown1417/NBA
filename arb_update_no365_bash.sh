18761db2bbfa1ad31c59cb349816c71558d9ab2a
jamesbrown1417 <james.brown1417@gmail.com>
2024-11-19 00:17:31 +1030

    latest

---
+++ arb_update_no365_bash.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Set the current directory to your project folder
+cd ~/Desktop/Projects/NBA || exit
+
+# Remove .json and .txt files in specific directories
+rm OddsScraper/Neds/*.json
+
+# Execute Python and R scripts
+python3 OddsScraper/TAB/get-TAB-response.py
+python3 OddsScraper/Neds/get_neds_urls.py
+Rscript OddsScraper/Neds/get_neds_match_urls.R
+python3 OddsScraper/Neds/get_match_json.py
+
+# Execute R script for getting arbs
+Rscript Scripts/get_arbs.R
+
+# Publish report using Quarto
+echo "1" | quarto publish quarto-pub Reports/nba_arbs.qmd
+
+# Automatically stage all changes
+git add .
+
+# Commit changes with a message including "automated commit" and the current timestamp
+commitMessage="automated commit and timestamp $(date '+%Y-%m-%d %H:%M:%S')"
+git commit -m "$commitMessage"
+
+# Push the commit to the 'main' branch on 'origin'
+git push origin main
