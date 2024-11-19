6bc91b068ca1751954c85a4b5809b9b6612ff485
James <james.brown1417@gmail.com>
2024-11-19 21:13:57 +1030

    automated commit and timestamp 2024-11-19 21:13:57

--- arb_update_no365_bash.sh
+++ arb_update_no365_bash.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Give access to normal path vars
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
 # Set the current directory to your project folder
 cd ~/Projects/NBA || exit
 
