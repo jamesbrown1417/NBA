cfbc42919eab0cf3bd3797289946546b16ae64ac
James <james.brown1417@gmail.com>
2024-12-01 12:46:31 +1030

    automated commit and timestamp 2024-12-01 12:46:31

--- OddsScraper/scrape_betkings.R
+++ OddsScraper/scrape_betkings.R
@@ -5,6 +5,42 @@ library(rvest)
 library(jsonlite)
 library(glue)
 
+# Get teams table
+teams <-
+  read_csv("Data/all_teams.csv")
+
+# Get player names table
+player_names_all <-
+  read_csv("Data/all_rosters.csv") |>
+  select(player_full_name = PLAYER, TeamID) |> 
+  left_join(teams[, c("id", "full_name")], by = c("TeamID" = "id")) |> 
+  mutate(first_initial = str_sub(player_full_name, 1, 1)) |>
+  mutate(surname = str_extract(player_full_name, "(?<=\\s).*$")) |> 
+  mutate(join_name = paste(first_initial, surname, sep = " ")) |> 
+  rename(team_name = full_name)
+
+# unique join names
+player_names_unique <-
+  player_names_all |>
+  group_by(join_name) |> 
+  filter(n() == 1) |> 
+  ungroup()
+
+# Non unique names (take first two letters of first name)
+player_names_non_unique <-
+  player_names_all |>
+  group_by(join_name) |> 
+  filter(n() > 1) |> 
+  mutate(first_initial = str_sub(player_full_name, 1, 2)) |>
+  mutate(join_name = paste(first_initial, surname, sep = " ")) |> 
+  ungroup()
+
+player_names <-
+  bind_rows(player_names_unique, player_names_non_unique) |> 
+  mutate(join_name = ifelse(player_full_name == "Keyontae Johnson", "Key Johnson", join_name)) |> 
+  mutate(join_name = ifelse(player_full_name == "Miles Bridges", "Mil Bridges", join_name)) |> 
+  mutate(join_name = ifelse(player_full_name == "Jaylin Williams", "Jay Williams", join_name))
+
 # Get main landing page url
 betkings_main_nba_url <- "https://www.betkings.com.au/sportsbook/Basketball/United%20States/NBA"
 
@@ -22,12 +58,12 @@ event_ids <- str_extract(betkings_main_nba_links, "(?<=\\?e=)\\d+")
 
 # Define URLs with meaningful names
 player_points_url <- glue("https://site-gateway.betkings.com.au/sportsbook/event/{event_ids}?includeMarketGroups=PLAYER_POINTS&locale=ENG")
-player_rebounds_url <- "https://site-gateway.betkings.com.au/sportsbook/event/{event_ids}?includeMarketGroups=PLAYER_REBOUNDS&locale=ENG"
-player_assists_url <- "https://site-gateway.betkings.com.au/sportsbook/event/{event_ids}?includeMarketGroups=PLAYER_ASSISTS&locale=ENG"
-player_blocks_url <- "https://site-gateway.betkings.com.au/sportsbook/event/{event_ids}?includeMarketGroups=PLAYER_BLOCKS&locale=ENG"
-player_steals_url <- "https://site-gateway.betkings.com.au/sportsbook/event/{event_ids}?includeMarketGroups=PLAYER_STEALS&locale=ENG"
-player_three_pointers_url <- "https://site-gateway.betkings.com.au/sportsbook/event/{event_ids}?includeMarketGroups=PLAYER_THREE_POINTERS&locale=ENG"
-player_par_url <- "https://site-gateway.betkings.com.au/sportsbook/event/{event_ids}?includeMarketGroups=PLAYER_POINTS_ASSISTS_AND_REBOUNDS&locale=ENG"
+player_rebounds_url <- glue("https://site-gateway.betkings.com.au/sportsbook/event/{event_ids}?includeMarketGroups=PLAYER_REBOUNDS&locale=ENG")
+player_assists_url <- glue("https://site-gateway.betkings.com.au/sportsbook/event/{event_ids}?includeMarketGroups=PLAYER_ASSISTS&locale=ENG")
+player_blocks_url <- glue("https://site-gateway.betkings.com.au/sportsbook/event/{event_ids}?includeMarketGroups=PLAYER_BLOCKS&locale=ENG")
+player_steals_url <- glue("https://site-gateway.betkings.com.au/sportsbook/event/{event_ids}?includeMarketGroups=PLAYER_STEALS&locale=ENG")
+player_three_pointers_url <- glue("https://site-gateway.betkings.com.au/sportsbook/event/{event_ids}?includeMarketGroups=PLAYER_THREE_POINTERS&locale=ENG")
+player_par_url <- glue("https://site-gateway.betkings.com.au/sportsbook/event/{event_ids}?includeMarketGroups=PLAYER_POINTS_ASSISTS_AND_REBOUNDS&locale=ENG")
 
 # Function to retrieve JSON data and save it to a named list
 get_json_data <- function(url) {
@@ -69,37 +105,52 @@ get_json_data <- function(url) {
 
 # Function to take a list of JSON data and extract the necessary information----
 process_props <- function(json_data) {
-  # Get the match name and market type
-  match_name <- json_data$name
-  market <- json_data$marketTypes[[1]]$name
-  markets <- json_data$marketTypes[[1]]$markets
   
-  # Loop over markets and extract player data
-  prop_name_1 <- c()
-  prop_name_2 <- c()
-  price_1 <- c()
-  price_2 <- c()
+  # Number of markets to iterate over
+  num_markets <- length(json_data$marketTypes)
   
-  for (i in seq_along(markets)) {
-    prop_name_1 <- c(prop_name_1, markets[[i]]$outcome[[1]]$name)
-    prop_name_2 <- c(prop_name_2, markets[[i]]$outcome[[2]]$name)
-    price_1 <- c(price_1, markets[[i]]$outcome[[1]]$odds)
-    price_2 <- c(price_2, markets[[i]]$outcome[[2]]$odds)
+  # list of length num_markets to store data
+  prop_data_list <- list()
+  
+  for (j in 1:num_markets) {
+    # Get the match name and market type
+    match_name <- json_data$name
+    market <- json_data$marketTypes[[j]]$name
+    markets <- json_data$marketTypes[[j]]$markets
+    
+    # Loop over markets and extract player data
+    prop_name_1 <- c()
+    prop_name_2 <- c()
+    price_1 <- c()
+    price_2 <- c()
+    
+    for (i in seq_along(markets)) {
+      prop_name_1 <- c(prop_name_1, markets[[i]]$outcome[[1]]$name)
+      prop_name_2 <- c(prop_name_2, markets[[i]]$outcome[[2]]$name)
+      price_1 <- c(price_1, markets[[i]]$outcome[[1]]$odds)
+      price_2 <- c(price_2, markets[[i]]$outcome[[2]]$odds)
+    }
+    
+    # Combine
+    prop_name <- c(prop_name_1, prop_name_2)
+    price <- c(price_1, price_2)
+    
+    # Create a tibble with the extracted data
+    prop_data <- tibble(
+      match_name = match_name,
+      market = market,
+      prop_name = prop_name,
+      price = price
+    )
+    
+    prop_data_list[[j]] <- prop_data
   }
   
-  # Combine
-  prop_name <- c(prop_name_1, prop_name_2)
-  price <- c(price_1, price_2)
+  output_tibble <- bind_rows(prop_data_list)
   
-  # Create a tibble with the extracted data
-  prop_data <- tibble(
-    match_name = match_name,
-    market = market,
-    prop_name = prop_name,
-    price = price
-  )
+  names(output_tibble) <- c("match_name", "market_name", "prop_name", "price")
   
-  return(prop_data)
+  return(output_tibble)
 }
 
 #===============================================================================
@@ -109,9 +160,12 @@ process_props <- function(json_data) {
 # Get JSON data for player points
 player_points_data <- map(player_points_url, get_json_data)
 
+# Get safe version of process_props
+process_props <- safely(process_props)
+
 # Extract data from JSON
 player_points_df <-
-  map(player_points_data, process_props) %>% 
+  map(player_points_data, process_props) %>%
   bind_rows()
 
 # Get Just Overs
@@ -119,7 +173,96 @@ player_points_overs <-
   player_points_df |>
   filter(str_detect(prop_name, "Over")) %>% 
   separate(prop_name, into = c("player_name", "line"), sep = " Over ") %>%
-  
+  mutate(line = str_remove(line, "\\(")) %>%
+  mutate(line = str_remove(line, "\\)")) %>%
+  mutate(line = as.numeric(line)) %>%
+  transmute(match = match_name, market_name = "Player Points", player_name, line, over_price = price)
 
+# Get Just Unders
+player_points_unders <-
+  player_points_df |>
+  filter(str_detect(prop_name, "Under")) %>% 
+  separate(prop_name, into = c("player_name", "line"), sep = " Under ") %>%
+  mutate(line = str_remove(line, "\\(")) %>%
+  mutate(line = str_remove(line, "\\)")) %>%
+  mutate(line = as.numeric(line)) %>%
+  transmute(match = match_name, market_name = "Player Points", player_name, line, under_price = price)
 
+# Combine
+player_points_all <-
+  player_points_overs %>%
+  left_join(player_points_unders) %>%
+  left_join(player_names[,c("player_full_name", "team_name")], by = c("player_name" = "player_full_name")) %>%
+  rename(player_team = team_name) %>% 
+  separate(match, into = c("home_team", "away_team"), sep = " vs ", remove = FALSE) %>%
+  mutate(opposition_team = ifelse(home_team == player_team, away_team, home_team)) %>%
+  mutate(match = paste(home_team, "v", away_team, sep = " ")) %>% 
+  transmute(
+    match,
+    home_team,
+    away_team,
+    market_name,
+    player_name,
+    player_team,
+    opposition_team,
+    line,
+    over_price,
+    under_price,
+    agency = "Sportsbet"
+  )
+
+#===============================================================================
+# Get player assists data
+#===============================================================================
+
+# Get JSON data for player assists
+player_assists_data <- map(player_assists_url, get_json_data)
+
+# Extract data from JSON
+player_assists_df <-
+  map(player_assists_data, process_props) %>% 
+  bind_rows()
+
+# Get Just Overs
+player_assists_overs <-
+  player_assists_df |>
+  filter(str_detect(prop_name, "Over")) %>% 
+  separate(prop_name, into = c("player_name", "line"), sep = " Over ") %>%
+  mutate(line = str_remove(line, "\\(")) %>%
+  mutate(line = str_remove(line, "\\)")) %>%
+  mutate(line = as.numeric(line)) %>%
+  transmute(match = match_name, market_name = "Player Assists", player_name, line, over_price = price)
+
+# Get Just Unders
+player_assists_unders <-
+  player_assists_df |>
+  filter(str_detect(prop_name, "Under")) %>% 
+  separate(prop_name, into = c("player_name", "line"), sep = " Under ") %>%
+  mutate(line = str_remove(line, "\\(")) %>%
+  mutate(line = str_remove(line, "\\)")) %>%
+  mutate(line = as.numeric(line)) %>%
+  transmute(match = match_name, market_name = "Player Assists", player_name, line, under_price = price)
+
+# Combine
+player_assists_all <-
+  player_assists_overs %>%
+  left_join(player_assists_unders) %>%
+  left_join(player_names[,c("player_full_name", "team_name")], by = c("player_name" = "player_full_name")) %>%
+  rename(player_team = team_name) %>% 
+  separate(match, into = c("home_team", "away_team"), sep = " vs ", remove = FALSE) %>%
+  mutate(opposition_team = ifelse(home_team == player_team, away_team, home_team)) %>%
+  mutate(match = paste(home_team, "v", away_team, sep = " ")) %>% 
+  transmute(
+    match,
+    home_team,
+    away_team,
+    market_name,
+    player_name,
+    player_team,
+    opposition_team,
+    line,
+    over_price,
+    under_price,
+    agency = "Sportsbet"
+  )
 
