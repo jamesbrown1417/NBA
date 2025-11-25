e053d8990b63c581b7b49440e683cf1fa42f1600
James <james.brown1417@gmail.com>
2025-11-26 08:41:18 +1030

    automated commit and timestamp 2025-11-26 08:41:18

--- OddsScraper/Bet365/get_bet365_html.py
+++ OddsScraper/Bet365/get_bet365_html.py
@@ -55,10 +55,10 @@ match_range = range(len(started), len(NBA_schedule))
 async def collect_h2h_and_urls(driver):
     """Navigate to main page, save H2H HTML, and return list of player URLs."""
     await driver.get('https://www.bet365.com.au/#/AC/B18/C20604387/D48/E1453/F10/')
-    await driver.sleep(200)
+    await driver.sleep(2)
     # Always perform login each run
     print("Attempting login...")
-    login_element = await driver.find_element(By.XPATH, "//div[contains(@class, 'hm-MainHeaderRHSLoggedOutWide_Login')]", timeout=10)
+    login_element = await driver.find_element(By.XPATH, "//div[contains(@class, 'hm-MainHeaderRHSLoggedOutWide_Login')] | //button[contains(@class, 'hrm-4') and contains(text(), 'Log In')]", timeout=10)
     await driver.sleep(2)
     await login_element.click()
     await driver.sleep(1)
