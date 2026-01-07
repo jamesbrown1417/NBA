d9edec4819a4a080f0ae6f37f9071c6edc4f8c88
James <james.brown1417@gmail.com>
2026-01-08 08:57:52 +1030

    automated commit and timestamp 2026-01-08 08:57:52

--- OddsScraper/Bet365/get_bet365_html.py
+++ OddsScraper/Bet365/get_bet365_html.py
@@ -87,7 +87,7 @@ async def collect_h2h_and_urls(driver):
     await driver.sleep(2)
     # Always perform login each run
     print("Attempting login...")
-    login_element = await driver.find_element(By.XPATH, "//div[contains(@class, 'hm-MainHeaderRHSLoggedOutWide_Login')] | //button[contains(@class, 'hrm-a') and contains(text(), 'Log In')]", timeout=10)
+    login_element = await driver.find_element(By.XPATH, "//div[contains(@class, 'hm-MainHeaderRHSLoggedOutWide_Login')] | //button[contains(@class, 'hrm-79') and contains(text(), 'Log In')]", timeout=10)
     await driver.sleep(2)
     await login_element.click()
     await driver.sleep(1)
